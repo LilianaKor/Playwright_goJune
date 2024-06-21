4150a2854d40b17caed92b0cc2692e5e68e82feb
Liliana <lilianakoric@gmail.com>
2024-06-20 22:11:22 -0700

    add test_saucedemo.py, test_example

--- tests/codegendemo.py
+++ tests/codegendemo.py
@@ -1,5 +1,5 @@
 import re
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page, expect, Playwright, sync_playwright
 
 
 def test_example(page: Page) -> None:
@@ -10,3 +10,23 @@ def test_example(page: Page) -> None:
     page.locator("[data-test=\"password\"]").fill("secret_sauce")
     page.locator("[data-test=\"login-button\"]").click()
     expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.webkit.launch(headless=False)
+    context = browser.new_context(**playwright.devices["iPhone 13"])
+    page = context.new_page()
+    page.goto("https://www.saucedemo.com/")
+    page.locator("[data-test=\"username\"]").click()
+    page.locator("[data-test=\"username\"]").fill("standard_user")
+    page.locator("[data-test=\"password\"]").click()
+    page.locator("[data-test=\"password\"]").fill("djshfoagjj")
+    page.locator("[data-test=\"login-button\"]").click()
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
